61e3a6d21b4735d207aa668ba3a7a5d870e1da5c
erikgustavalm <erikgustavalm@gmail.com>
2017-06-07 18:42:22 +0200

    commenting done

--- setup.c
+++ setup.c
@@ -1,12 +1,16 @@
 #include "setup.h"
 
+
+// The initalize process
 void initSetup()
 {
   
   printf("Initialize game\n");
 
+  // Calls for SDL to init everything from audio to event
   SDL_Init(SDL_INIT_EVERYTHING);
-  
+
+  // Creating a window with title "HELLO WORLD", positioned in the center of screen, with macrodefined size
   gameWindow = SDL_CreateWindow("HELLO WORLD", SDL_WINDOWPOS_CENTERED,
 				SDL_WINDOWPOS_CENTERED,SCREEN_SIZE, SCREEN_SIZE,
 				SDL_WINDOW_SHOWN);
@@ -16,7 +20,7 @@ void initSetup()
     printf("gameWindow couldn't be created\n");
 
   }else {
- 
+    // If creating of window is succesfull we create the render and set it to be hardwareaccelerated(using the dedicated graphics card)
     gameRender = SDL_CreateRenderer(gameWindow,
 				    -1,
 				    SDL_RENDERER_ACCELERATED);
@@ -25,36 +29,57 @@ void initSetup()
       printf("gameRender couldn't be created\n");
 
     }else {
-
-      SDL_SetRenderDrawColor(gameRender, 255,255,255,255);
       
+      SDL_SetRenderDrawColor(gameRender, 255,255,255,255);
+
+      // Gamestate will be set to menu in the begining
       gamestate = MENU;
       gameloop();
 
     }
   }
+
+  // quit the game 
   quit();
 }
 
+// Gamelooptroop rockers
 void gameloop()
 {
-  while (gamestate == MENU){
+  // while gamestate is anything but QUIT the loop will be running with eventchecking, logical operations and rendering
+  while (gamestate != QUIT) {
+
+    updateLogic();
     eventCheck();
     drawScreen();
+    
   }
 }
 
+//
+void updateLogic()
+{
+
+}
+
+// SDL_PollEvent returns a buffer of events happening
 void eventCheck()
 {
   SDL_Event event;
   while (SDL_PollEvent(&event)) {
     switch (event.type) {
+      // If event is the X window button being pressed gamestate is changed to QUIT 
       case SDL_QUIT:
 	  gamestate = QUIT;
 	break;
-      case SDL_KEYDOWN: 
-	  switch (event.key.keysym.sym) {
+	
+      // If a key is pressed down	
+      case SDL_KEYDOWN:
 
+	// we check wich key is pressed
+	  switch (event.key.keysym.sym) {
+	    
+	    // if the key = ESC, gamestate = QUIT 
 	     case SDLK_ESCAPE: 
 	       gamestate = QUIT;
 	     break;
@@ -70,17 +95,35 @@ void eventCheck()
   }
 }
 
+// Screen is drawn
 void drawScreen()
 {
+  // Clear screan 
   SDL_RenderClear(gameRender);
+
+  // draw menuspecific items if gamestate is MENU
+  if (gamestate == MENU) {
+    
+  }
   
+  // draw gameplay
+  if (gamestate == PLAY) {
+    
+  }
+
+  // Update screen to next position in buffer
   SDL_RenderPresent(gameRender);
 }
+
+// Quitting function that cleans up and destroys pointers
 void quit()
 {
   SDL_DestroyWindow(gameWindow);
+  gameWindow = NULL;
+  
   SDL_DestroyRenderer(gameRender);
-
+  gameRender = NULL;
+  
   SDL_Quit();
   
   printf("game executed fine\n");
--- setup.h
+++ setup.h
@@ -4,18 +4,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Define a macro for screen size, both width and height
 #define SCREEN_SIZE 640
 
 
+// Gamestate is to be used as identifier for which mode the game will be in
+// and what to do thereafter
 typedef enum{ MENU, QUIT, PLAY} gamestate_t;
-
-SDL_Renderer* gameRender;
-SDL_Window* gameWindow;
-
 gamestate_t gamestate;
 
+// gameRender is the rendering structure/engine that will be used whenever
+//an object needs to be drawn to screen
+SDL_Renderer* gameRender;
+
+// gameWindow is simply the instance of a window, only used in init and exit
+SDL_Window* gameWindow;
+
+
+// First function to be called and only initalize everything else
 extern void initSetup();
+
+// The body of the whole, inside everything happends
 extern void gameloop();
+
+// A function for a nice cleanup of resources
 extern void quit();
+
+// Where logical operations is called from
+extern void updateLogic();
+
+// Checking for all events that might be happening
 extern void eventCheck();
+
+// The rendering of object to the screen
 extern void drawScreen();
